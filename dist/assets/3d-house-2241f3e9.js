565b1f75c45002eb41a1d56f91052afadc8013c8
Nikhil Sharma <nikhilsharma@Nikhils-MacBook-Air.local>
2023-06-30 12:29:50 +0530

    Nik changes 1

--- dist/assets/3d-house-2241f3e9.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("3d-house-1d28acaf.svg",import.meta.url).href;export{o as h};
--- dist/assets/omit-68f9a7f9.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./reactNode-5a95d97c.js";function c(a,r){var o=e({},a);return Array.isArray(r)&&r.forEach(function(t){delete o[t]}),o}export{c as o};
